e36b56d8d5670e5086eecdc6a91c5cdc9fa4718a
Aditya Kumar Choudhary <adityakrchy101@gmail.com>
2025-01-02 13:05:26 +0530

    added sitemap and robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://impactlab.in', // Replace with your website's URL
+    generateRobotsTxt: true,          // Automatically generate `robots.txt`
+    sitemapSize: 5000,                // Optional: split sitemap if it exceeds this size
+  };
+  
